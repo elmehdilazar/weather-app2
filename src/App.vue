d2d4d34c461ae48cbefb3eafb7b307a29cbdc39f
elmehdilazar <96011628+elmehdilazar@users.noreply.github.com>
2023-10-23 14:11:32 +0100

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<div class="app">
+  <div class="header container h-100 p-5">
+    <h1 class="mb-5">Weather App</h1>
+    <div class="d-flex justify-content-center h-100">
+
+<div class="searcbar w-50 mx-2">
+  <input type="text" class="input form-control" placeholder="Enter a City">
+</div>
+<button type="button" class="btn-search btn btn-primary">Search <i class="fa fa-search" aria-hidden="true"></i></button>
+
+    </div>
+  </div>
+</div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body{
+  background-color: #121212 !important;
+
 }
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.header{
+  background-color: #212730;
+  border-radius: 20px;
+  margin-top: 5rem;
+  color: #fff;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  text-align: center;
+}
+.btn-search{
+  background-image: linear-gradient(to right,cyan,magenta);
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// import 'bootstrap'
+// import "bootstrap-vue"
+ import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
 createApp(App).use(store).use(router).mount('#app')
